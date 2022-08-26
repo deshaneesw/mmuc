777f005cd506c19002d9997dd780025050673392
Deshanee Wickramarachchi <deshanee.sw@gmail.com>
2022-08-26 13:13:21 +0800

    Update

--- R/muc.R
+++ R/muc.R
@@ -88,16 +88,29 @@
 #'
 muc <- function(y,a,m,cv,Data,conf.level=0.95,a.link="logit") {
   if(missing(Data)) {
+    if(missing(cv)){
+      Data<-data.frame(get(y),get(a),mget(m,inherits = T))
+      names(Data)<-c(y,a,m)
+      } else {
     Data<-data.frame(get(y),get(a),mget(m,inherits = T),mget(cv,inherits = T))
     names(Data)<-c(y,a,m,cv)
+      }
     }
-  var<-c(y,a,m,cv)
+  if(missing(cv)){
+    var<-c(y,a,m)
+    nc<-0
+  } else {
+    var<-c(y,a,m,cv)
+    nc<-length(cv)
+  }
   Dt <- stats::na.omit(Data[,var])
   n0<-nrow(Data)
   n<-nrow(Dt)
   nmed<-length(m)
-  nc<-length(cv)
-  C<-as.matrix(Dt[,cv])
+  if(missing(cv)) {
+    C<-as.matrix(rep(1,n))
+  } else C<-cbind(1,as.matrix(Dt[,cv]))
+
   ## estimating functions
   inv.link<-function(x,link){
     switch(link, logit=1/(1+exp(-x)), identity=x, log=exp(x),
@@ -108,29 +121,29 @@ muc <- function(y,a,m,cv,Data,conf.level=0.95,a.link="logit") {
   ng<-1+nc+(2+nc)*nmed+1+nmed
   est<-function(g){
     h<-rep(0,ng);
-    pi<-inv.link(as.vector(cbind(1,C)%*%g[1:(1+nc)]),link=a.link)
+    pi<-inv.link(as.vector(C%*%g[1:(1+nc)]),link=a.link)
     beta<-g[(2+nc):(1+nc+(2+nc)*nmed)]
     theta_1<-g[1+nc+(2+nc)*nmed+1]
     theta_2<-g[(nc+(2+nc)*nmed+3):(nc+(2+nc)*nmed+2+nmed)]
-    h[1:(1+nc)]<-t(cbind(1,C))%*%(Dt[,a]-pi)
-    h[(2+nc):(1+nc+(2+nc)*nmed)]<-as.vector(t(cbind(1,Dt[,a],C))%*%as.matrix(Dt[,m]-cbind(1,Dt[,a],C)%*%matrix(beta,ncol=nmed)))
+    h[1:(1+nc)]<-t(C)%*%(Dt[,a]-pi)
+    h[(2+nc):(1+nc+(2+nc)*nmed)]<-as.vector(t(cbind(Dt[,a],C))%*%as.matrix(Dt[,m]-cbind(Dt[,a],C)%*%matrix(beta,ncol=nmed)))
     h[1+nc+(2+nc)*nmed+1]<-t(Dt[,a]-pi)%*%(Dt[,y]-Dt[,a]*theta_1-as.matrix(Dt[,m])%*%theta_2)
-    h[(2+nc+(2+nc)*nmed+1):(2+nc+(2+nc)*nmed+nmed)]<- t(Dt[,a]-pi)%*%as.matrix((Dt[,m]-cbind(1,Dt[,a],C)%*%matrix(beta,ncol=nmed))*as.vector(Dt[,y]-as.matrix(Dt[,m])%*%theta_2))
+    h[(2+nc+(2+nc)*nmed+1):(2+nc+(2+nc)*nmed+nmed)]<- t(Dt[,a]-pi)%*%as.matrix((Dt[,m]-cbind(Dt[,a],C)%*%matrix(beta,ncol=nmed))*as.vector(Dt[,y]-as.matrix(Dt[,m])%*%theta_2))
     h
   }
 
   est.fun<-function(g){
-    pi<-inv.link(as.vector(cbind(1,C)%*%g[1:(1+nc)]),link=a.link)
+    pi<-inv.link(as.vector(C%*%g[1:(1+nc)]),link=a.link)
     beta<-g[(2+nc):(1+nc+(2+nc)*nmed)]
     theta_1<-g[1+nc+(2+nc)*nmed+1]
     theta_2<-g[(nc+(2+nc)*nmed+3):(nc+(2+nc)*nmed+2+nmed)]
-    f<-cbind(1,C)*(Dt[,a]-pi)
+    f<-C*(Dt[,a]-pi)
     for (i in 1:nmed) {
-      f<-cbind(f,cbind(1,Dt[,a],C)*as.vector(Dt[,m[i]]-cbind(1,Dt[,a],C)%*%(matrix(beta,ncol=nmed)[,i])))
+      f<-cbind(f,cbind(Dt[,a],C)*as.vector(Dt[,m[i]]-cbind(Dt[,a],C)%*%(matrix(beta,ncol=nmed)[,i])))
     }
     f<-cbind(f,(Dt[,a]-pi)*(Dt[,y]-Dt[,a]*theta_1-as.matrix(Dt[,m])%*%theta_2))
     for (i in 1:nmed) {
-      f<-cbind(f,(Dt[,a]-pi)*((Dt[,m[i]]-cbind(1,Dt[,a],C)%*%(matrix(beta,ncol=nmed)[,i]))*(Dt[,y]-as.matrix(Dt[,m])%*%theta_2)))
+      f<-cbind(f,(Dt[,a]-pi)*((Dt[,m[i]]-cbind(Dt[,a],C)%*%(matrix(beta,ncol=nmed)[,i]))*(Dt[,y]-as.matrix(Dt[,m])%*%theta_2)))
     }
     f
   }
@@ -142,7 +155,7 @@ muc <- function(y,a,m,cv,Data,conf.level=0.95,a.link="logit") {
   run$fn.reduction
   sol<-run[1]
   nde<-sol$par[1+nc+(2+nc)*nmed+1]
-  nie.m<-sol$par[seq(3+nc,(1+nc+(2+nc)*nmed),2+nc)]*sol$par[seq(3+nc+(2+nc)*nmed,1+nc+(2+nc)*nmed+1+nmed,1)]
+  nie.m<-sol$par[seq(2+nc,(1+nc+(2+nc)*nmed),2+nc)]*sol$par[seq(3+nc+(2+nc)*nmed,1+nc+(2+nc)*nmed+1+nmed,1)]
   nie<-sum(nie.m)
 
   G<-numDeriv::jacobian(func=est,x=sol$par)/n
@@ -154,17 +167,21 @@ muc <- function(y,a,m,cv,Data,conf.level=0.95,a.link="logit") {
   #variance of nie
   h<-rep(0,1+nc)
   for (i in seq(3+nc+(2+nc)*nmed,1+nc+(2+nc)*nmed+1+nmed,1)) {
-    h<-append(h,c(0,sol$par[i],rep(0,nc)))
+    h<-append(h,c(sol$par[i],rep(0,1+nc)))
   }
   h<-append(h,0)
-  for (i in seq(3+nc,(1+nc+(2+nc)*nmed),2+nc)) {
+  for (i in seq(2+nc,(1+nc+(2+nc)*nmed),2+nc)) {
     h<-append(h,sol$par[i])
   }
   var_nie<-t(h)%*%(solve(G)%*%(t(om)%*%om/n)%*%t(solve(G))/n)%*%t(t(h))
   se.nie<-sqrt(var_nie)
+  if(missing(cv)){
+    out<-list(nde=nde,nie=nie,se.nde=se.nde,se.nie=se.nie,
+              conf.level=conf.level,y=y,cv="None",a=a,m=m,n0=n0,n=n,nmed=nmed,nc=nc,conv=conv)
 
-  out<-list(nde=nde,nie=nie,se.nde=se.nde,se.nie=se.nie,
-            conf.level=conf.level,y=y,cv=cv,a=a,m=m,n0=n0,n=n,nmed=nmed,nc=nc,conv=conv)
+  } else {out<-list(nde=nde,nie=nie,se.nde=se.nde,se.nie=se.nie,
+                   conf.level=conf.level,y=y,cv=cv,a=a,m=m,n0=n0,n=n,nmed=nmed,nc=nc,conv=conv)}
+
   class(out)<-"muc"
   out
 }
